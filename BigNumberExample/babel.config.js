71a823263a596456d6257c647681a01fc0fcb731
Hanno J. Gödecke <die.drei99@yahoo.de>
2023-04-06 09:07:50 +0200

    made big number 0.71 compatible

---
+++ BigNumberExample/babel.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const pak = require('../package.json');
+
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          [pak.name]: path.join(__dirname, '..', pak.source),
+          bn_elliptic: path.join(__dirname, 'src', 'bn-elliptic', 'lib'),
+          stream: 'stream-browserify',
+        },
+      },
+    ],
+  ],
+};
