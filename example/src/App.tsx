4d4e59163667575cf5940c2c53b8f0b5313e624f
Renan Mav <renan.mav@hotmail.com>
2025-01-31 15:15:02 -0300

    chore: log new achitecture

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import { Root } from './navigators/Root';
 
+// @ts-expect-error
+const isNewArchitectureEnabled = Boolean(global?.nativeFabricUIManager);
+console.log('New Architecture:', isNewArchitectureEnabled);
+
 export default function App() {
   return <Root />;
 }
